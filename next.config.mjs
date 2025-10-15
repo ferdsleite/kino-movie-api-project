8ee5b668d2c7628629cd7c134de26d3e3dbae060
Fernanda <ferdsleite@gmail.com>
2025-10-15 16:04:47 +0200

    movies and actors pages created and Home redirected

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: "/",
+                destination: "/movies",
+                permanent: true
+            }
+        ]
+    }
+};
 export default nextConfig;
