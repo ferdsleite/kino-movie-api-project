62e30df43b53a3bcdf26752ae20b0fa9bf2c8abb
Fernanda <ferdsleite@gmail.com>
2025-10-23 11:44:36 +0200

    page.tsx adjusted to deploy

--- src/app/movies/[id]/page.tsx
+++ src/app/movies/[id]/page.tsx
@@ -4,14 +4,13 @@ import MovieSuggestions from "@/components/movies/MovieSuggestions";
 import Wrap from "@/components/template/Wrap";
 import { getMovieDetails } from "@/lib/MovieAPI";
 
-export default async function Movie({
-    params,
-    }: {
-    params: { id: string };
-    }) {
+interface MovieProps {
+  params: Promise<{ id: string }>;
+}
 
-    const { id } = params;
-    const movieDetails:MovieDetails =  await getMovieDetails(String(id));
+export default async function Movie({ params }: MovieProps) {
+  const { id } = await params;
+  const movieDetails: MovieDetails = await getMovieDetails(String(id));
 
     return (
         <Wrap>
